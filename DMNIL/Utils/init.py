5897be1c5dadd4c16b9c5d27b91a66d209ed7dab
Indifferent <140402123+ISChenawei@users.noreply.github.com>
2025-09-23 18:50:56 +0800

    Add files via upload

---
+++ DMNIL/Utils/init.py
@@ -0,0 +1,79 @@
+# Original copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: CC-BY-NC-4.0
+from __future__ import absolute_import
+import torch.nn as nn
+
+
+def c2_xavier_fill(module: nn.Module) -> None:
+    """
+    Initialize `module.weight` using the "XavierFill" implemented in Caffe2.
+    Also initializes `module.bias` to 0.
+
+    Args:
+        module (torch.nn.Module): module to initialize.
+    """
+    # Caffe2 implementation of XavierFill in fact
+    # corresponds to kaiming_uniform_ in PyTorch
+    nn.init.kaiming_uniform_(module.weight, a=1)  # pyre-ignore
+    if module.bias is not None:  # pyre-ignore
+        nn.init.constant_(module.bias, 0)
+
+
+def c2_msra_fill(module: nn.Module) -> None:
+    """
+    Initialize `module.weight` using the "MSRAFill" implemented in Caffe2.
+    Also initializes `module.bias` to 0.
+
+    Args:
+        module (torch.nn.Module): module to initialize.
+    """
+    # pyre-ignore
+    nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="relu")
+    if module.bias is not None:  # pyre-ignore
+        nn.init.constant_(module.bias, 0)
+
+
+def normal_init(module: nn.Module, std=0.01):
+    nn.init.normal_(module.weight, std=std)
+    if module.bias is not None:
+        nn.init.constant_(module.bias, 0)
+
+
+def init_weights(module, init_linear='normal'):
+    assert init_linear in ['normal', 'kaiming'], \
+        "Undefined init_linear: {}".format(init_linear)
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            if init_linear == 'normal':
+                normal_init(m, std=0.01)
+            else:
+                c2_msra_fill(m)
+        elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.GroupNorm, nn.SyncBatchNorm)):
+            if m.weight is not None:
+                nn.init.constant_(m.weight, 1)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Conv1d):
+            c2_msra_fill(m)
+
+class IterLoader:
+    def __init__(self, loader, length=None):
+        self.loader = loader
+        self.length = length
+        self.iter = None
+
+    def __len__(self):
+        if self.length is not None:
+            return self.length
+
+        return len(self.loader)
+
+    def new_epoch(self):
+        self.iter = iter(self.loader)
+
+    def next(self):
+        try:
+            return next(self.iter)
+        except:
+            self.iter = iter(self.loader)
+            return next(self.iter)
