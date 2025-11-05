3cdca3ede2ea64c4800e401d88a8eaf8851e0d5e
Indifferent <140402123+ISChenawei@users.noreply.github.com>
2025-11-05 09:19:44 +0800

    Update evaluators.py

--- DMNIL/evaluators.py
+++ DMNIL/evaluators.py
@@ -8,7 +8,6 @@ import random
 import copy
 from torch.cuda.amp import autocast
 from tqdm import tqdm
-from .evaluation_metrics import cmc,mean_ap
 from .Utils.meters import AverageMeter
 from .Utils.rerank import re_ranking
 import torch.nn.functional as F
@@ -70,79 +69,3 @@ def extract_features(model, data_loader, print_freq=50,flip=True):
                                 data_time.val, data_time.avg))
         print("total time: {}".format(time.time()-end))
         return features,features_s#, labels
-
-
-def pairwise_distance(features, query=None, gallery=None):
-    if query is None and gallery is None:
-        n = len(features)
-        x = torch.cat(list(features.values()))
-        x = x.view(n, -1)
-        dist_m = torch.pow(x, 2).sum(dim=1, keepdim=True) * 2
-        dist_m = dist_m.expand(n, n) - 2 * torch.mm(x, x.t())
-        return dist_m
-
-    x = torch.cat([features[f].unsqueeze(0) for f, _, _ in query], 0)
-    y = torch.cat([features[f].unsqueeze(0) for f, _, _ in gallery], 0)
-    m, n = x.size(0), y.size(0)
-    x = x.view(m, -1)
-    y = y.view(n, -1)
-    dist_m = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(m, n) + \
-           torch.pow(y, 2).sum(dim=1, keepdim=True).expand(n, m).t()
-    dist_m.addmm_(1, -2, x, y.t())
-    return dist_m, x.numpy(), y.numpy()
-
-
-def evaluate_all(query_features, gallery_features, distmat, query=None, gallery=None,
-                 query_ids=None, gallery_ids=None,
-                 query_cams=None, gallery_cams=None,
-                 cmc_topk=(1, 5, 10), cmc_flag=False,regdb=False):
-    if query is not None and gallery is not None:
-        query_ids = [pid for _, pid, _ in query]
-        gallery_ids = [pid for _, pid, _ in gallery]
-        query_cams = [cam for _, _, cam in query]
-        gallery_cams = [cam for _, _, cam in gallery]
-    else:
-        assert (query_ids is not None and gallery_ids is not None
-                and query_cams is not None and gallery_cams is not None)
-
-    # Compute mean AP
-    mAP = mean_ap(distmat, query_ids, gallery_ids, query_cams, gallery_cams,regdb=regdb)
-    print('Mean AP: {:4.1%}'.format(mAP))
-
-    if (not cmc_flag):
-        return mAP
-
-    cmc_configs = {
-        'market1501': dict(separate_camera_set=False,
-                           single_gallery_shot=False,
-                           first_match_break=True),}
-    cmc_scores = {name: cmc(distmat, query_ids, gallery_ids,
-                            query_cams, gallery_cams,regdb=regdb, **params)
-                  for name, params in cmc_configs.items()}
-
-    print('CMC Scores:')
-    for k in cmc_topk:
-        print('  top-{:<4}{:12.1%}'.format(k, cmc_scores['market1501'][k-1]))
-    return cmc_scores['market1501'], mAP
-
-
-class Evaluator(object):
-    def __init__(self, model):
-        super(Evaluator, self).__init__()
-        self.model = model
-
-    def evaluate(self, data_loader, query, gallery, cmc_flag=False, rerank=False,modal=0,regdb=False):
-        features, _ = extract_features(self.model, data_loader,mode=modal)
-        # print(features,features) 
-        distmat, query_features, gallery_features = pairwise_distance(features, query, gallery)
-        # print(distmat)
-        results = evaluate_all(query_features, gallery_features, distmat, query=query, gallery=gallery, cmc_flag=cmc_flag,regdb=regdb)
-
-        if (not rerank):
-            return results
-
-        print('Applying person re-ranking ...') 
-        distmat_qq, _, _ = pairwise_distance(features, query, query)
-        distmat_gg, _, _ = pairwise_distance(features, gallery, gallery)
-        distmat = re_ranking(distmat.numpy(), distmat_qq.numpy(), distmat_gg.numpy())
-        return evaluate_all(query_features, gallery_features, distmat, query=query, gallery=gallery, cmc_flag=cmc_flag)
